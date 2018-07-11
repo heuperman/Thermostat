e1aabffa8b7b0dbe7006c779adab2546c1434f32
Cornelis Heuperman <cheuperman@gmail.com>
2018-07-11 15:41:18 +0100

    added min temp, throws error when below min temp

--- spec/unit/thermostatSpec.js
+++ spec/unit/thermostatSpec.js
@@ -2,21 +2,31 @@
 
 describe('Thermostat', function() {
 
+var thermostat;
+
   beforeEach(function(){
-    this.thermostat = new Thermostat();
+    thermostat = new Thermostat();
   });
 
   it("starts at 20 degrees", function() {
-    expect(this.thermostat.temperature).toEqual(20);
+    expect(thermostat.temperature).toEqual(20);
   });
 
   it("increases temperature", function() {
-    this.thermostat.up(3);
-    expect(this.thermostat.temperature).toEqual(23);
+    thermostat.up(3);
+    expect(thermostat.temperature).toEqual(23);
   });
 
   it("increases temperature", function() {
-    this.thermostat.down(3);
-    expect(this.thermostat.temperature).toEqual(17);
+    thermostat.down(3);
+    expect(thermostat.temperature).toEqual(17);
+  });
+
+  it("it has a minimum temperature of 10 degrees", function() {
+    expect(thermostat.minTemp).toEqual(10);
+  });
+
+  it("throws an error when user sets temperature below minimum", function() {
+    expect(function() { thermostat.down(12); }).toThrowError('Cannot reduce temperature: temperature too low');
   });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -2,6 +2,7 @@
 
 function Thermostat() {
   this.temperature = 20;
+  this.minTemp = 10;
 }
 
 Thermostat.prototype.up = function(degrees){
@@ -9,5 +10,8 @@ Thermostat.prototype.up = function(degrees){
 };
 
 Thermostat.prototype.down = function(degrees){
+  if((this.temperature - degrees) < this.minTemp) {
+    throw new Error('Cannot reduce temperature: temperature too low');
+  }
   this.temperature -= degrees;
 };
